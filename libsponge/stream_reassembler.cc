a66cebcdb77e02afcf3400e9085b2e2928a974b6
Briidge <low1932219008@qq.com>
2022-07-30 17:06:17 +0800

    Lab1: 基于unordered_map / map<uint64_t, char>的流重组实现

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -17,10 +17,59 @@ StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity),
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
-void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+void StreamReassembler::push_substring(const string &data, const uint64_t index, const bool eof) {
+    if (eof) _eof_flag = true;
+    if (!data.empty())
+        _total_bytes = max(index + data.size(), _total_bytes);
+    // 1.将数据写入暂存区
+    size_t j = _next_expect_index > index ? _next_expect_index - index : 0;
+    for (; j < data.size() && unassembled_bytes() + _output.buffer_size() < _capacity && j + index < _capacity - _output.buffer_size() + _next_expect_index; j++) {
+        if (_map.find(j + index) == _map.end()) {
+            _map[j + index] = data[j];
+        }
+    }
+//    for (auto iter = _data.begin(); iter != _data.end() && j < data.size() && (_output.buffer_size() + unassembled_bytes() < _capacity); iter++) {
+//        // 判断在整个字节流中，索引
+//        if ((*iter).first + _next_expect_index >= j + index) {
+//            if ((*iter).first + _next_expect_index > j + index)
+//                iter = _data.insert(iter, pair<uint64_t, char>(index + j, data[j]));
+//            j++;
+//        }
+//    }
+    // 在_data末尾插入剩余的数据
+//    for (; j < min(data.size(), _capacity - _output.buffer_size() + _next_expect_index - index) && (_output.buffer_size() + unassembled_bytes() < _capacity); j++) {
+//        _data.emplace_back(pair<uint64_t, char>(index + j, data[j]));
+//    }
+
+    // 2.暂存区数据写入字节流
+    std::string str;
+    while (!_map.empty() && _map.find(_next_expect_index) != _map.end()) {
+        str.push_back(_map[_next_expect_index]);
+        _map.erase(_next_expect_index);
+        _next_expect_index++;
+    }
+//    bool in = false;
+//    while (!_data.empty() && _next_expect_index >= _data.front().first) {
+//        in = true;
+//        if (_next_expect_index == _data.front().first) {
+//            str.push_back(_data.front().second);
+//            _next_expect_index++;
+//        }
+//        _data.pop_front();
+//    }
+//    if (in)
+//        _output.write(str);
+    if (!str.empty())
+        _output.write(str);
+    if (_eof_flag && _output.bytes_written() == _total_bytes) {
+        _output.end_input();
+    }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const {
+    return _map.size();
+}
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const {
+    return unassembled_bytes() == 0;
+}
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,6 +5,8 @@
 
 #include <cstdint>
 #include <string>
+#include <list>
+#include <unordered_map>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
@@ -14,6 +16,11 @@ class StreamReassembler {
 
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
+    bool _eof_flag = false;
+    bool over = false;
+    uint64_t _next_expect_index = 0;
+    uint64_t _total_bytes = 0;
+    std::unordered_map<uint64_t, char> _map{};
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
