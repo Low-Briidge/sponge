9346572a4c5de28a7729fa241264386d7cde37d5
Briidge <low1932219008@qq.com>
2022-07-31 21:04:01 +0800

    ByteStream optimize

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -15,50 +15,42 @@ using namespace std;
 ByteStream::ByteStream(const size_t _capacity) : capacity(_capacity){}
 
 size_t ByteStream::write(const string &data) {
-    size_t count = 0;
-    for (char c : data) {
-        if (this->size < this->capacity) {
-            stream.push_back(c);
-            size++;
-            have_written++;
-            count++;
-        }
-        else {
-            break;
-        }
+    size_t len = data.length();
+    if (len > capacity - _buffer.size()) {
+        len = capacity - _buffer.size();
     }
-    return count;
+    have_written += len;
+    _buffer.append(BufferList(move(string().assign(data.begin(),data.begin()+len))));
+    return len;
 }
 
 //! \param[in] len bytes will be copied from the output side of the buffer
 string ByteStream::peek_output(const size_t len) const {
-    size_t _len = len;
-    if (len > this->size) _len = this->size;
-    string res;
-    auto it = this->stream.begin();
-    for (size_t i = 0; i < _len; i++) {
-        res += *it++;
+    size_t length = len;
+    if (length > _buffer.size()) {
+        length = _buffer.size();
     }
-    return res;
+    string s=_buffer.concatenate();
+    return string().assign(s.begin(), s.begin() + length);
 }
 
 //! \param[in] len bytes will be removed from the output side of the buffer
 void ByteStream::pop_output(const size_t len) {
-    size_t _len = len;
-    if (len > this->size) _len = this->size;
-    size -= _len;
-    have_read += _len;
-    for (size_t i = 0; i < _len; i++) {
-        this->stream.pop_front();
+    size_t length = len;
+    if (length > _buffer.size()) {
+        length = _buffer.size();
     }
+    have_read += length;
+    _buffer.remove_prefix(length);
+    return;
 }
 
 //! Read (i.e., copy and then pop) the next "len" bytes of the stream
 //! \param[in] len bytes will be popped and returned
 //! \returns a string
 std::string ByteStream::read(const size_t len) {
-    size_t _len = len > this->size ? this->size : len;
-    std::string res = peek_output(_len);
+    size_t _len = len > buffer_size() ? buffer_size() : len;
+    string res = peek_output(_len);
     pop_output(_len);
     return res;
 }
@@ -72,11 +64,11 @@ bool ByteStream::input_ended() const {
 }
 
 size_t ByteStream::buffer_size() const {
-    return this->stream.size();
+    return this->_buffer.size();
 }
 
 bool ByteStream::buffer_empty() const {
-    return size == 0;
+    return _buffer.size() == 0;
 }
 
 bool ByteStream::eof() const {
@@ -92,5 +84,5 @@ size_t ByteStream::bytes_read() const {
 }
 
 size_t ByteStream::remaining_capacity() const {
-    return this->capacity - this->size;
+    return this->capacity - buffer_size();
 }
